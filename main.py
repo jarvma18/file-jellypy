2815e412f47f231928c509c4b7a099965facdbbd
jarvma18 <markusmjarvinen@outlook.com>
2023-03-19 12:42:14 +0200

    Refactored code to different functions to reduce complexity.

--- main.py
+++ main.py
@@ -12,17 +12,41 @@ def buildJsonObject(keys, values):
     jsonObject[keys[x]] = valuesList[x]
   return jsonObject
 
+def csvToJson(file):
+  csvLines = file.readlines()
+  csvKeys = csvLines[0]
+  keysList = splitValuesFromCSV(csvKeys)
+  csvLines.pop(0)
+  jsonArray = [];
+  for x in range(len(csvLines)):
+    jsonObject = buildJsonObject(keysList, csvLines[x])
+    jsonArray.append(jsonObject)
+  return jsonArray
+
+def appendDataFromJsonToCsv(x, jsonData, csvFile):
+  strToAppend = '';
+  if x == 0:
+    for key in jsonData[x]:
+      if len(strToAppend) == 0:
+        strToAppend += key
+      else:
+        strToAppend += ',' + key
+    strToAppend += '\n'
+    csvFile.write(strToAppend)
+    strToAppend = ''
+  for key in jsonData[x]:
+    if len(strToAppend) == 0:
+      strToAppend += jsonData[x][key]
+    else:
+      strToAppend += ',' + jsonData[x][key]
+  strToAppend += '\n'
+  csvFile.write(strToAppend)
+  return
+
 def main(fileName, convertMode):
   file = open(fileName, 'r')
   if convertMode == 'csvToJson':
-    csvLines = file.readlines()
-    csvKeys = csvLines[0]
-    keysList = splitValuesFromCSV(csvKeys)
-    csvLines.pop(0)
-    jsonArray = [];
-    for x in range(len(csvLines)):
-      jsonObject = buildJsonObject(keysList, csvLines[x])
-      jsonArray.append(jsonObject)
+    jsonArray = csvToJson(file)
     jsonFileName = fileName.split('.')[0] + '.json'
     jsonFile = open(jsonFileName, 'w')
     json.dump(jsonArray, jsonFile, ensure_ascii=False, indent=2)
@@ -31,22 +55,7 @@ def main(fileName, convertMode):
     csvFileName = fileName.split('.')[0] + '.csv'
     csvFile = open(csvFileName, 'a')
     for x in range(len(jsonData)):
-      strToAppend = '';
-      if x == 0:
-        for key in jsonData[x]:
-          if len(strToAppend) == 0:
-            strToAppend += key
-          else:
-            strToAppend += ',' + key
-        strToAppend += '\n'
-        csvFile.write(strToAppend)
-      for key in jsonData[x]:
-        if len(strToAppend) == 0:
-          strToAppend += jsonData[x][key]
-        else:
-          strToAppend += ',' + jsonData[x][key]
-      strToAppend += '\n'
-      csvFile.write(strToAppend)
+      appendDataFromJsonToCsv(x, jsonData, csvFile)
   print('Job done')
 
 if sys.argv[1] and sys.argv[2]:
